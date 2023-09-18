86748cf56b94bc7a84fd5a28e80e864d68f130e4
Kevin <kevpadilla01@gmail.com>
2023-09-17 22:12:25 -0300

    feat: nav component added

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -8,30 +8,38 @@ function HamburgerMenu () {
   }
 
   return (
-    <div className='md:hidden z-0'>
-      <div className='' onClick={toggleModal}>
-        {modalOpen
-          ? <svg xmlns='http://www.w3.org/2000/svg' width='20' height='21'><g fill='#D0D6F9' fillRule='evenodd'><path d='M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z' /><path d='M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z' /></g></svg>
-          : <svg className='z-20' xmlns='http://www.w3.org/2000/svg' width='24' height='21'><g fill='#D0D6F9' fillRule='evenodd'><path d='M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z' /></g></svg>}
-      </div>
-      {modalOpen && (
-        <div className='fixed inset-0' onClick={toggleModal}>
-          <div className='flex flex-col items-left gap-5 pt-[100px] pl-[32px] ml-auto h-full w-[254px] text-DarkBlue bg-White bg-[#ffffff0a] backdrop-blur-[40px]'>
-            <a href='#' className='uppercase w-full flex items-center gap-[11px] border-r-4 border-white active:border-white hover:border-white/50 transition-all ease-in-out'>
-              <span className='font-bold'>00</span> Home
-            </a>
-            <a href='#' className='uppercase w-full flex items-center gap-[11px] transition-all ease-in-out'>
-              <span className='font-bold'>01</span> Destination
-            </a>
-            <a href='#' className='uppercase w-full flex items-center gap-[11px] transition-all ease-in-out'>
-              <span className='font-bold'>02</span> Crew
-            </a>
-            <a href='#' className='uppercase w-full flex items-center gap-[11px] transition-all ease-in-out'>
-              <span className='font-bold'>03</span> Technology
-            </a>
+    <div className='px-8 md:hidden'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between'>
+          <input type='checkbox' name='hamburger' id='hamburger' className='peer' hidden />
+          <label htmlFor='hamburger' className='peer-checked:hamburger block relative z-20 p-6 -mr-6 cursor-pointer lg:hidden'>
+            {modalOpen
+              ? <svg onClick={toggleModal} xmlns='http://www.w3.org/2000/svg' width='20' height='21'><g fill='#D0D6F9' fillRule='evenodd'><path d='M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z' /><path d='M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z' /></g></svg>
+              : <svg onClick={toggleModal} xmlns='http://www.w3.org/2000/svg' width='24' height='21'><g fill='#D0D6F9' fillRule='evenodd'><path d='M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z' /></g></svg>}
+          </label>
+
+          <div className='peer-checked:translate-x-0 translate-x-full transition duration-300 fixed inset-0 flex justify-end'>
+            <div className='w-[calc(254px)] bg-[#ffffff0a] backdrop-blur-[40px]'>
+              <div className='flex flex-col items-left gap-5 pt-[100px] pl-[32px] ml-auto'>
+                <a href='#' className='uppercase w-full flex items-center gap-[11px] border-r-4 border-white active:border-white hover:border-white/50 transition-all ease-in-out'>
+                  <span className='font-bold'>00</span> Home
+                </a>
+                <a href='#' className='uppercase w-full flex items-center gap-[11px] transition-all ease-in-out'>
+                  <span className='font-bold'>01</span> Destination
+                </a>
+                <a href='#' className='uppercase w-full flex items-center gap-[11px] transition-all ease-in-out'>
+                  <span className='font-bold'>02</span> Crew
+                </a>
+                <a href='#' className='uppercase w-full flex items-center gap-[11px] transition-all ease-in-out'>
+                  <span className='font-bold'>03</span> Technology
+                </a>
+              </div>
+
+            </div>
           </div>
         </div>
-      )}
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import HamburgerMenu from './HamburguerMenu'
+
+function Nav () {
+  return (
+    <header className='relative h-[96px] text-white lg:mt-[40px] ml-[39px] mr-[26px] md:mr-0 lg:ml-[55px] flex justify-between items-center'>
+      <div className=' w-[48px] h-[48px] '>
+        <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48'>
+          <g fill='none' fillRule='evenodd'>
+            <circle cx='24' cy='24' r='24' fill='#FFF' />
+            <path fill='#0B0D17' d='M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z' />
+          </g>
+        </svg>
+      </div>
+      <div className='hidden md:block flex-1 relative h-full'>
+        <div className='absolute top-1/2 -right-[30px] w-full z-20 h-[1px] bg-white/25 max-w-[473px]' />
+      </div>
+      <div className='w-[450px] lg:w-[700px] xl:w-[830px] h-full bg-[#ffffff0a] backdrop-blur-[40px] hidden md:flex items-center justify-center'>
+        <nav className='flex gap-6 lg:gap-[48px] h-full align-middle'>
+          <a href='#' className='uppercase h-full flex items-center gap-[11px] border-b-4 border-white active:border-white hover:border-white/50 transition-all ease-in-out'>
+            <span className='font-bold hidden lg:block'>00</span> Home
+          </a>
+          <a href='#' className='uppercase h-full flex items-center gap-[11px] border-b-4 border-white active:border-white hover:border-white/50 transition-all ease-in-out'>
+            <span className='font-bold hidden lg:block'>01</span> Destination
+          </a>
+          <a href='#' className='uppercase h-full flex items-center gap-[11px] border-b-4 border-white active:border-white hover:border-white/50 transition-all ease-in-out'>
+            <span className='font-bold hidden lg:block'>02</span> Crew
+          </a>
+          <a href='#' className='uppercase h-full flex items-center gap-[11px] border-b-4 border-white active:border-white hover:border-white/50 transition-all ease-in-out'>
+            <span className='font-bold hidden lg:block'>03</span> Technology
+          </a>
+        </nav>
+      </div>
+      <HamburgerMenu />
+    </header>
+  )
+}
+
+export default Nav
