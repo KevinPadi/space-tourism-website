824fcc659816a71c326f4d6a1f540c976e22d379
Kevin <kevpadilla01@gmail.com>
2023-09-29 16:29:46 -0300

    fix: cta button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'wouter'
 
 function HomePage () {
   return (
@@ -11,9 +12,12 @@ function HomePage () {
             Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
           </p>
         </div>
-        <div className=' transition-shadow duration-300 hover:shadow-[0px_1px_0px_100px_rgba(255,255,255,0.15)] cursor-pointer relative flex items-center justify-center w-[150px] md:w-[242px] md:h-[242px] h-[150px] bg-white rounded-full mx-auto lg:mx-0 peer'>
-          <a className='font-["Bellefair"] text-[20px] md:text-[32px] text-darkBlue tracking-[1.25px] uppercase' href='#'>Explore</a>
-          <div className='absolute w-full h-full bg-white/60 rounded-full -z-10 peer-hover:scale-105' />
+        <div className=' '>
+          <Link href='/destination'>
+            <a className='transition-shadow duration-300 hover:shadow-[0px_1px_0px_100px_rgba(255,255,255,0.15)] cursor-pointer relative flex items-center justify-center  mx-auto lg:mx-0 peer w-[150px] md:w-[242px] md:h-[242px] h-[150px] bg-white rounded-full font-["Bellefair"] text-[20px] md:text-[32px] text-darkBlue tracking-[1.25px] uppercase'>
+              Explore
+            </a>
+          </Link>
         </div>
       </div>
     </main>
